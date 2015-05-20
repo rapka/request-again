330246e1645edf65b94eab810e4999d36dfa76e6
Colin Hemphill <colin@hemphill.us>
2015-05-20 11:23:25 -0500

    Getting and setting cache. Call request.

--- request-again.js
+++ request-again.js
@@ -1,31 +1,88 @@
 /* REQUIREMENTS */
+
 var request = require('request');
+var lru = require('lru-cache');
 
 /* DEFAULTS */
+
 var defaults = {
-  expires: 300000
+  max: 50,
+  maxAge: 1000 * 60 * 1
 };
 
 /* CONSTRUCTORS */
-var requestAgain = function(param1, param2, param3) {
+
+var RequestAgain = function() {
   var self = this;
+  self.cacheOptions = defaults;
+  self.cache = lru(defaults);
+};
 
-  // handle constructor overloading
-  var url, options, fn;
-  if (typeof param1 === 'string') {
-    url = param1;
-  } else if (typeof param1 === 'object') {
-    options = param1;
-    url = param2;
+module.exports = new RequestAgain();
+
+/* PROTOTYPE FUNCTIONS */
+
+RequestAgain.prototype.enableCache = function(options) {
+  var self = this;
+  self.cacheOptions = options;
+  self.cache = lru(options);
+};
+
+RequestAgain.prototype.cached = function(param1, param2, param3) {
+  var self = this;
+  var url, options, callback;
+  url = param1;
+  options = param2;
+  callback = param3;
+
+  var cachedResponse = self.getCache(url, options);
+  if (cachedResponse) {
+    return callback(null, null, cachedResponse);
   }
 
-  // handle constructor errors
-  if (!url) {
-    throw new Error('Error calling request - URL string is undefined or malformed.');
+  request(url, options, function(err, res, body) {
+    if (err) {
+      return callback(err, null, null);
+    }
+    self.setCache(url, options, body);
+    return callback(err, res, body);
+  });
+};
+
+RequestAgain.prototype.setCache = function(url, options, value) {
+  var self = this;
+  if (self.cache) {
+    var key = genKey(url, options);
+    return self.cache.set(key, value);
+  } else {
+    throw new Error('Cache not set on request-again object. You may have not constructed it properly.');
   }
+};
 
-}
+RequestAgain.prototype.getCache = function(url, options) {
+  var self = this;
+  if (self.cache) {
+    var key = genKey(url, options);
+    return self.cache.get(key);
+  } else {
+    throw new Error('Cache not set on request-again object. You may have not constructed it properly.');
+  }
+};
 
-module.exports = requestAgain;
+RequestAgain.prototype.resetCache = function() {
+  var self = this;
+  if (self.cache) {
+    return self.cache.reset();
+  } else {
+    throw new Error('Cache not set on request-again object. You may have not constructed it properly.');
+  }
+};
 
 /* HELPERS */
+
+function genKey(url, options) {
+  return JSON.stringify({
+    url: url,
+    options: options
+  });
+}
--- tests/requestAgain.js
+++ tests/requestAgain.js
@@ -1,4 +1,23 @@
 /* Requirements */
 var chai = require('chai');
 var expect = chai.expect;
-var requestCached = require('../request-again');
+
+var request = require('../request-again');
+
+request.enableCache({
+  max: 100,
+  maxAge: 1000 * 60 * 1
+});
+
+function makeTheCall() {
+  var startTime = Date.now();
+  request.cached('http://pokeapi.co/api/v1/pokemon/1', {}, function(err, res, body) {
+    // console.log('err::', err);
+    // console.log('body::', body);
+    return console.log('It took:', Date.now() - startTime);
+  });
+}
+
+makeTheCall();
+
+setTimeout(makeTheCall, 2000);
